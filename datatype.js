7d3ee8161bca5637babac337e1f1998274b8ca93
Princi Kardam <princikardam017@gmail.com>
2022-12-15 19:38:18 +0530

    first message

---
+++ datatype.js
@@ -0,0 +1,16 @@
+
+
+let e = 10;
+let d = 40;
+console.log("eg of Number datatypes are" , e, d);
+console.log("Addition of e and d is" , e+d);
+
+const b = 2002;
+console.log(b);
+
+let a = "i love my colloege MMMUT, which is the best college in india";
+console.log(a.split(",")); 
+ let str="i love my college MMMMUT, which is the best college in India and going to be IIT";
+ console.log(str.split(","));
+
+
---
+++ index.js
@@ -0,0 +1,53 @@
+console.log("hello world");//is used for printing.
+
+//for integer
+let a=25;
+console.log(a,"number");
+
+//for string
+let b="princi";
+console.log(b,"string");
+
+//for array
+ let c=[1,2,3,4,5];
+ console.log(c,"array");
+
+//  addition of no
+let p=2;
+let q=5;
+console.log("addition of two numbers;",p+q);
+
+// for subtraction
+let d=7;
+let e=5;
+console.log("sub of two numbers;",d-e);
+
+// for multiplication
+let u=2;
+let v=5;
+console.log("multiplication of two numbers;",u*v);
+
+// for division
+let s=7;
+let t=5;
+console.log("division of two numbers;",s/t);
+
+
+let m="Princi ";
+let n="Kardam";
+console.log("addition ",m+n);
+
+
+
+let x = 10;
+let y = 40;
+console.log("eg of Number datatypes are" , x,y);
+console.log("Addition of e and d is" , x+y);
+
+const z = 2002;
+console.log(z);
+
+let w = "i love my colloege MMMUT, which is the best college in india";
+console.log( w.split(","));
+console.log(w.slice(10));
+console.log( w.charAt(10));
