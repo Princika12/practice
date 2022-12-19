6cf417de90bb1bc4995bb86ca0a41aad995cb0e0
Princika12 <119876905+Princika12@users.noreply.github.com>
2022-12-19 21:04:15 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,67 @@
+// const tutorialcopy10rs = {
+//     name:"Princi Kardam",
+//     branch:"EE",
+//     rollno:"2021031165",
+//     session:"2021-2025",
+//     college:"MMMUT,Gkp",
+    
+// };
+// console.log(tutorialcopy10rs["name"]);
+// console.log(tutorialcopy10rs["branch"]);
+// console.log(tutorialcopy10rs["rollno"]);
+// console.log(tutorialcopy10rs["session"]);
+// console.log(tutorialcopy10rs["college"]);
+
+// whenever we want to store data which is in the form of key value pairs, then
+// Object is used
+// keys should never have spaces between them and 
+// should not start with any number or special characters
+// if it has spaces, then key should be enclosed within parenthesis "" (string form) [for eg="first name"]
+
+// accessing values of object
+// console.log(tutorialcopy10rs["name"]);
+// OTHER WAY:
+// console.log(tutorialcopy10rs.name);
+// console.log(tutorialcopy10rs.branch);
+// console.log(tutorialcopy10rs.rollno);
+// console.log(tutorialcopy10rs.session);
+// console.log(tutorialcopy10rs.college);
+
+
+
+// const login = {
+//     username: "princi017",
+//     password: "princi017",
+//     contactNo: "8869809275",
+//     address:" Meerut",
+//     pincode: "35557",
+// };
+//  console.log(login);
+//  console.log(login["username"]);
+//  console.log(login["password"]);
+//  console.log(login.address);
+//  console.log(login.pincode);
+
+// to get all the keys of an object 
+
+
+
+const email = {
+   firstname: "princi017",
+     lastname: "kardam@017",
+     contactNo: "8869809275",
+     from:"princi",
+     to: "kardam",
+};
+
+console.log(email);
+  console.log(email["firstname"]);
+  console.log(email["lastname"]);
+  console.log(email.contactNo);
+  console.log(email.from);
+  console.log(email.to);
+
+
+
+ 
+
