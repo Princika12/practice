0a26cca032dcb0e9663918b3fad69b509400ebf2
Princika12 <119876905+Princika12@users.noreply.github.com>
2022-12-20 21:50:53 +0530

    Add files via upload

---
+++ nestedobject.js
@@ -0,0 +1,24 @@
+const biodata = {
+    name: "Princi Kardam",
+    class: 10,
+    school: "MPGS",
+    location: {
+        state: "UP",
+        city: "MEERUT",
+        pincode: 250004,
+    },
+};
+
+// nested object is an object in which one of he value of any key is
+// again an object
+console.log(biodata.location);
+// for printing patricular information from an object
+console.log(biodata.location.pincode);
+
+
+const response = 
+for(let a of response){
+    console.log(a.file.originalname);
+}
+
+
