16051cd65ad47fb4e7f78da56ee1baf1b9758608
Princika12 <119876905+Princika12@users.noreply.github.com>
2022-12-21 21:58:27 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,71 @@
+// whenever we see a set of codes getting repeated or needed
+// continuously then we define a function, which contains
+// that repeatedly codes inside it. Now whenever we want that codes
+// we call that function and get those codes
+
+
+// without functions
+
+// car1 
+// console.log("Engine work of car1");
+// console.log("Mechanical work of car1");
+// console.log("Interior work of car1");
+// console.log("security work of car1");
+// console.log("Car 1 is manufactured");
+
+// car2
+// console.log("Engine work of car2");
+// console.log("Mechanical work of car2");
+// console.log("Interior work of car2");
+// console.log("security work of car2");
+// console.log("Car 2 is manufactured");
+
+// car3
+// console.log("Engine work of car3");
+// console.log("Mechanical work of car3");
+// console.log("Interior work of car3");
+// console.log("security work of car3");
+// console.log("Car 3 is manufactured");
+
+
+// with functions   (fat arrow)
+// const enginefunction = ()=>{
+//     console.log("Engine work is done");
+// };
+
+// const mechanicalfunction = ()=>{
+//     console.log("mechanical work is done");
+// };
+
+// const interiorfunction = ()=>{
+//     console.log("interior work is done");
+// };
+
+// const securityfunction = ()=>{
+//     console.log("security work is done");
+// };
+
+// Car 1
+// enginefunction();
+// mechanicalfunction();
+// interiorfunction();
+// securityfunction();
+// console.log("Car 1 is manufactured");
+
+// Car 2
+// enginefunction();
+// mechanicalfunction();
+// interiorfunction();
+// securityfunction();
+// console.log("Car 2 is manufactured");
+
+// Car 3
+// enginefunction();
+// mechanicalfunction();
+// interiorfunction();
+// securityfunction();
+// console.log("Car 3 is manufactured");
+
+
+// date-21/12/2022
+
